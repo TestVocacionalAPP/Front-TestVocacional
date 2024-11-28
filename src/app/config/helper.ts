859766b790293cb5d0d8a72beb262d9c4c1c3ded
ssalvaq <167903441+ssalvaq@users.noreply.github.com>
2024-11-27 21:09:04 -0500

    Update helper.ts

--- src/app/config/helper.ts
+++ src/app/config/helper.ts
@@ -1,3 +1,3 @@
-let baserUrl = 'https://back-testvocacional-hidden-morning-4648.fly.dev'
+let baserUrl = 'https://test-backend-v1.fly.dev'
 //let baserUrl = 'http://localhost:8080'
 export default baserUrl;
